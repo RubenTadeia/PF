61468d9398bc50f597a034656c0b61eda6a298ce
ZeDiogo <zediogooliveira@hotmail.com>
2017-04-13 04:39:36 +0100

    repair mean

--- Lab2/Grupo 8/myMean.m
+++ Lab2/Grupo 8/myMean.m
@@ -1,15 +1,16 @@
 function Mean = myMean(file)
     fin = fopen(file, 'r');
-    size = 0;
+    Size = 0;
     Mean = 0;
     while ~feof(fin)
-        line = fgets(fin);
-        if (line(1)~=0)
-            size = size + 1;
-            Mean = Mean + line(1);
+        Line = fgets(fin);
+        Splitted = strsplit(Line,' ');
+        if (str2num(cell2mat(Splitted(1))) ~= 0)
+            Size = Size + 1;
+            Mean = Mean + str2num(cell2mat(Splitted(1)));
         end
     end
-    Mean = Mean/size;
+    Mean = Mean/Size
     fclose(fin);
 end
 
