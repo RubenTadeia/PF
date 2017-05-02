a0020b02ff4065d7bcfe99783a27144b77e29fb0
Ruben Tadeia <Ruben Tadeia>
2017-05-02 22:45:13 +0100

    Starting of part 4

---
+++ Lab3/Grupo8/Part2_Formant_Synthesis/FormantSynthesis.m
@@ -0,0 +1,12 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%          Instituto Superior Técnico          %
+%                                              %
+%             Speech Processing                %
+%                                              %
+%               Laboratório - 3                %
+%                                              %
+%                  Grupo 8                     %
+%                                              %
+%      Student - José  Diogo    - Nº 75255     %
+%      Student - Rúben Tadeia   - Nº 75268     %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
