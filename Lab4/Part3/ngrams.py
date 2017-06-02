d11913928e867482971a3fb270d42ddb91bf1156
ZeDiogo <zediogooliveira@hotmail.com>
2017-06-02 18:45:08 +0100

    added ngrams part3

--- Lab4/Part3/ngrams.py
+++ Lab4/Part3/ngrams.py
@@ -4,21 +4,142 @@
 # Processamento da Fala 2016/2017
 # Grupo 8
 #
-import shlex
+# import shlex
 
-# Open Training File
-with open("s1.txt", "rt") as fin:
+#Import for sorting
+import operator
 
-    for line in fin
-            print line
-    #with open("ngrams_s1.txt", "wt") as fout:
+def doUnigrams(fNameIn, fNameOut):
+	#Dictionary of unigrams
+	unigrams={}
 
-    #for line in fin:	    	n="[" + sys.argv[1] + "]"
-    	#if n in line:
-        	#fout.write(line.replace(line, ""))
-    	#else:
-    		#fout.write(line)
+	# Open Training File
+	with open(fNameIn, "rt") as fin:
+
+		for line in fin:	    	
+			words=line.split()
+			for word in words:
+				if word in unigrams:
+					unigrams[word] += 1
+				else:
+					unigrams[word] = 1 
 
 
-# Open Testing File
-#f = open('s2.txt', 'r')
+	#Sorting by value
+	sortedUnigrams = reversed(sorted(unigrams.items(), key=operator.itemgetter(1)))
+
+	#Writing sorted unigrams to file
+	with open(fNameOut, "wt") as fout:
+		numberUnigramsToPrint=10
+		n=0
+		frequentUnigrams=[]
+		for k in sortedUnigrams:
+			unigram=str(k[0]) + " " + str(k[1])
+			if n < numberUnigramsToPrint: 
+				print unigram
+				frequentUnigrams.append(unigram)
+				n += 1
+			fout.write(unigram + "\n")
+
+	return frequentUnigrams
+
+
+def doBigrams(fNameIn, fNameOut):
+
+	#Dictionary of bigrams
+	bigrams={}
+
+	# Open Training File
+	with open(fNameIn, "rt") as fin:
+	
+		for line in fin:	    	
+			words = line.split()
+			for i in range(0, len(words)-1):
+				bigram = words[i] + "_" + words[i+1]
+
+				if bigram in bigrams:
+					bigrams[bigram] += 1
+				else:
+					bigrams[bigram] = 1 
+
+
+	#Sorting by value
+	sortedBigrams = reversed(sorted(bigrams.items(), key=operator.itemgetter(1)))
+
+	#Writing sorted unigrams to file
+	with open(fNameOut, "wt") as fout:
+		numberBigramsToPrint=10
+		n=0
+		frequentBigrams=[]
+		for k in sortedBigrams:
+			bigram=str(k[0]) + " " + str(k[1])
+			if n < numberBigramsToPrint: 
+				print bigram
+				frequentBigrams.append(bigram)
+				n += 1
+			fout.write(bigram + "\n")
+
+	return frequentBigrams
+
+
+def fileLength(fname):
+	with open(fname, "rt") as f:
+		for i, l in enumerate(f):
+			pass
+	return i + 1
+
+
+def doVocabularySize():
+
+	print "ola"
+
+
+def doOutputFile(frequentUnigrams, frequentBigrams, numberUnigrams, numberBigrams, vocabSize):
+
+	with open("ngrams_s1.txt", "wt") as fout:
+		fout.write("Made by Group 8:\n")
+
+		fout.write("\n")
+		fout.write("Vocabulary size:\n")
+		fout.write(str(vocabSize)+"\n")
+
+		fout.write("\n")
+		fout.write("Number of unigrams\n")
+		fout.write(str(numberUnigrams)+"\n")
+
+		fout.write("\n")
+		fout.write("10 most frequent unigrams\n")
+		for i in frequentUnigrams:
+			fout.write(i+"\n")
+		
+		fout.write("\n")
+		fout.write("Number of bigrams\n")
+		fout.write(str(numberBigrams)+"\n")
+
+		fout.write("\n")
+		fout.write("10 most frequent bigrams\n")
+		for i in frequentBigrams:
+			fout.write(i+"\n")
+
+def numberOfNgrams(fname):
+	with open(fname, "rt") as f:
+		count = 0
+		for line in f:
+			value = line.split()
+			count += int(value[1])
+	return count
+
+
+def main():
+	frequentUnigrams = doUnigrams("s1.txt", "unigrams_s1.txt")
+	frequentBigrams = doBigrams("s1.txt", "bigrams_s1.txt")
+
+	numberUnigrams = numberOfNgrams("unigrams_s1.txt")
+	numberBigrams = numberOfNgrams("bigrams_s1.txt")
+	vocabSize = fileLength("unigrams_s1.txt")
+
+	doOutputFile(frequentUnigrams, frequentBigrams, numberUnigrams, numberBigrams, vocabSize)
+
+
+if __name__ == "__main__":
+	main()
