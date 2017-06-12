63dc6088acb567c1e6b6f5f0ae7f860a3f783f2e
ZeDiogo <zediogooliveira@hotmail.com>
2017-06-12 16:53:39 +0100

    last update

--- Lab4/Part2/tools_grid/scripts/do_allMixturesAndSpeakerType.sh
+++ Lab4/Part2/tools_grid/scripts/do_allMixturesAndSpeakerType.sh
@@ -5,8 +5,7 @@
 #========================================================
 
 #set of features to recognize
-# SET="test/"
-SET="dev"
+SET="test/"
 
 echo "##################################################"
 echo " "
@@ -14,7 +13,7 @@ echo "  RECOGNITION FOR 3 MIX AND SPEAKER INDEPENDENT"
 echo " "
 echo "##################################################"
 
-./scripts/do_recogInput.sh mfcc train3mix devSI features/mfcc/$SET 3mix independent
+./scripts/do_recogInput.sh mfcc train3mix testSI features/mfcc/$SET 3mix independent
 
 echo "##################################################"
 echo " "
@@ -22,7 +21,7 @@ echo "  RECOGNITION FOR 5 MIX AND SPEAKER INDEPENDENT"
 echo " "
 echo "##################################################"
 
-./scripts/do_recogInput.sh mfcc train5mix devSI features/mfcc/$SET 5mix independent
+./scripts/do_recogInput.sh mfcc train5mix testSI features/mfcc/$SET 5mix independent
 
 echo "##################################################"
 echo " "
@@ -30,7 +29,7 @@ echo "  RECOGNITION FOR 7 MIX AND SPEAKER INDEPENDENT"
 echo " "
 echo "##################################################"
 
-./scripts/do_recogInput.sh mfcc train7mix devSI features/mfcc/$SET 7mix independent
+./scripts/do_recogInput.sh mfcc train7mix testSI features/mfcc/$SET 7mix independent
 
 echo "##################################################"
 echo " "
@@ -38,7 +37,7 @@ echo "  RECOGNITION FOR 3 MIX AND SPEAKER ADAPTED"
 echo " "
 echo "##################################################"
 
-./scripts/do_recogInput.sh mfcc train3mix devSA features/mfcc/$SET 3mix adapted
+./scripts/do_recogInput.sh mfcc train3mix testSA features/mfcc/$SET 3mix adapted
 
 echo "##################################################"
 echo " "
@@ -46,7 +45,7 @@ echo "  RECOGNITION FOR 5 MIX AND SPEAKER ADAPTED"
 echo " "
 echo "##################################################"
 
-./scripts/do_recogInput.sh mfcc train5mix devSA features/mfcc/$SET 5mix adapted
+./scripts/do_recogInput.sh mfcc train5mix testSA features/mfcc/$SET 5mix adapted
 
 echo "##################################################"
 echo " "
@@ -54,4 +53,4 @@ echo "  RECOGNITION FOR 7 MIX AND SPEAKER ADAPTED"
 echo " "
 echo "##################################################"
 
-./scripts/do_recogInput.sh mfcc train7mix devSA features/mfcc/$SET 7mix adapted
+./scripts/do_recogInput.sh mfcc train7mix testSA features/mfcc/$SET 7mix adapted
